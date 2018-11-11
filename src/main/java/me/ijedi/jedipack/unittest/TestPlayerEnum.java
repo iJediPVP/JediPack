c076a16bd9fc958669c526846794e41adbf8f6ee
i_Jedi <ijedi325@gmail.com>
2018-11-11 16:13:03 -0600

    Begin the initial set up for unit testing

---
+++ src/main/java/me/ijedi/jedipack/unittest/TestPlayerEnum.java
@@ -0,0 +1,34 @@
+package me.ijedi.jedipack.unittest;
+
+import org.bukkit.entity.Player;
+import org.powermock.api.mockito.PowerMockito;
+
+import java.util.UUID;
+
+public enum TestPlayerEnum {
+
+    I_JEDI("i_Jedi", UUID.fromString("edb708e2-afb8-42c3-a365-7014245bf114"));
+
+    private String name;
+    private UUID playerId;
+
+    TestPlayerEnum(String name, UUID playerId){
+        this.name = name;
+        this.playerId = playerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public Player getMockPlayer(){
+        Player mockPlayer = PowerMockito.mock(Player.class);
+        PowerMockito.when(mockPlayer.getName()).thenReturn(name);
+        PowerMockito.when(mockPlayer.getUniqueId()).thenReturn(playerId);
+        return mockPlayer;
+    }
+}
