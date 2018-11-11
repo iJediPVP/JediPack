858a482dbe47dd7f822dc28d5270064b242f3e16
i_Jedi <ijedi325@gmail.com>
2018-11-11 16:55:58 -0600

    Set up automatic deployments for dev

---
+++ util/DevDeploy.ps1
@@ -0,0 +1,9 @@
+﻿param([String]$NewJar)
+
+$NewJar = "C:\Projects\JediPack\target\JediPack-1.0-SNAPSHOT.jar"
+
+$DestDir = "C:\Servers\Spigot 1.13.2\plugins"
+$ExistingFiles = Get-ChildItem -Path $DestDir -File  | ? { $_.Name.ToUpper().StartsWith("JEDIPACK") -and $_.Extension -eq ".jar" }
+$ExistingFiles | ForEach-Object { Remove-Item $_.FullName -Force -ErrorAction Stop }
+
+Copy-Item $NewJar $DestDir -Force -ErrorAction Stop
